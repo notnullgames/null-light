95328a76780c859c5604c508e60514e53c2b7805
David Konsumer <konsumer@jetboystudio.com>
2023-05-29 14:32:03 -0700

    get basic draw_image animation

--- carts/draw_image/main.nim
+++ carts/draw_image/main.nim
@@ -1,12 +1,17 @@
 var logo: Image
+var offset: Vector2
 
 load:
   trace("Hello from draw_image")
   logo = load_image("icon.png")
+  # this throws  malformed Wasm binary
+  #offset = (screen.dimensions() / 2) - (logo.dimensions() / 2)
+  offset.x = 96
 
 update:
   clear(BLACK)
-  logo.draw(vec2(96, 51))
+  offset.y = (int32(gameTime * 1000) mod (240+137)) - 137
+  logo.draw(offset)
 
 unload:
   trace("Ok, bye.")
