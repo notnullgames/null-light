ca4daff40702cd85775cfc143accf9910d4996c5
David Konsumer <konsumer@jetboystudio.com>
2023-06-04 18:58:02 -0700

    something wrong with fps

--- carts/draw_text/main.nim
+++ carts/draw_text/main.nim
@@ -9,8 +9,9 @@ update:
   clear(RAYWHITE)
   draw_text("default text", vec2(115, 80))
   draw_text("murder text", vec2(0, 0), vec2(320, 240), fontMurder, 0, CenterAlign, MiddleAlign)
+  
   # calling fps() here throws error: [trap] unreachable executed
-  #draw_text($fps(), vec2(0, 10), vec2(310, 230), 0, 0, RightAlign, TopAlign)
+  # draw_text($fps(), vec2(0, 10), vec2(310, 230), 0, 0, RightAlign, TopAlign)
 
   
 
